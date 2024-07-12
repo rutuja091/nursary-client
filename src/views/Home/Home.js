52db8a9b99eece50732eff899373d70ab67eced0
rutuja091 <gaikwadrutuja254@gmail.com>
2024-07-12 12:13:26 +0530

    react hot tost added

---
+++ src/views/Home/Home.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react'
+import "./Home.css"
+import PlantCard from "../../components/PlantCard/PlantCard"
+import axios from "axios"
+import toast,{Toaster} from "react-hot-toast"
+function Home() {
+    const [plants,setPlants]=useState([])
+
+    const loadPlants = async () => {
+      toast.loading("Loading Plants...")
+        const response = await axios.get('http://localhost:8000/plants')
+
+        toast.dismiss()
+
+
+        toast.success("Plants Loaded Successfully")
+
+        setPlants(response.data.data)
+    }
+
+    useEffect(()=>{
+  
+        loadPlants()
+    },[])
+
+  return ( 
+    <div>
+      <h1>Plants </h1>
+      {
+        plants.map((plant,i)=>{
+            const {
+                _id,
+                name,
+                category,
+                price,
+                image,
+                description
+             }=plant
+            return (<PlantCard
+                 key={i}
+                 _id={_id} 
+                 name={name} 
+                 category={category} 
+                 price={price} 
+                 image={image} 
+                 description={description}/>)
+        })
+      }
+      {
+
+    
+      }
+      <Toaster/>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/views/PlantView/PlantView.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./PlantView.css"
+function PlantView() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PlantView
